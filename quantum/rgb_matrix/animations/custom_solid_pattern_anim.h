b16b9944123caf709b55001200117264616041a5
mmartijnNL <35734805+mmartijnNL@users.noreply.github.com>
2023-06-27 15:12:27 +0200

    Using switch case for custom_solid_pattern_anim.h

--- quantum/rgb_matrix/animations/custom_solid_pattern_anim.h
+++ quantum/rgb_matrix/animations/custom_solid_pattern_anim.h
@@ -2,71 +2,48 @@
 RGB_MATRIX_EFFECT(CUSTOM_SOLID_PATTERN)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-bool CUSTOM_SOLID_PATTERN(effect_params_t* params) {
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+static HSV BAND_SPIRAL_SAT_math(HSV hsv, int16_t dx, int16_t dy, uint8_t dist, uint8_t time) {
+    hsv.s = scale8(hsv.s + dist - time - atan2_8(dy, dx), hsv.s);
+    return hsv;
+}
 
-    RGB rgb1 = rgb_matrix_hsv_to_rgb(rgb_matrix_config.hsv);
-    RGB rgb2;
+bool CUSTOM_SOLID_PATTERN(effect_params_t* params) {
+
+    return effect_runner_dx_dy_dist(params, &BAND_SPIRAL_SAT_math);
+
+    // Get the base color
+    HSV hsv1 = rgb_matrix_config.hsv;
+    RGB rgb1 = rgb_matrix_hsv_to_rgb(hsv1);
+    // Get the highlight color
+    HSV hsv2;
+    hsv2.h = hsv1.h-85;
+    hsv2.s = hsv1.s;
+    hsv2.v = hsv1.v;
+    RGB rgb2 = rgb_matrix_hsv_to_rgb(hsv2);
     rgb2.r = 0;
     rgb2.g = 200;
     rgb2.b = 255;
 
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+
     for (uint8_t i = led_min; i < led_max; i++) {
         RGB_MATRIX_TEST_LED_FLAGS();
-        rgb_matrix_set_color(i, rgb1.r, rgb1.g, rgb1.b);
+        switch (i)
+        {
+            case 5:     // F5
+            case 6:     // F6
+            case 42:    // F7
+            case 43:    // F8
+            case 18:    // W
+            case 24:    // A
+            case 25:    // S
+            case 26:    // D
+                rgb_matrix_set_color(i, rgb2.r, rgb2.g, rgb2.b);
+                break;
+            default:
+                rgb_matrix_set_color(i, rgb1.r, rgb1.g, rgb1.b);
+        }
     }
-
-    // F5 - F6
-    rgb_matrix_set_color(5, rgb2.r, rgb2.g, rgb2.b);
-    rgb_matrix_set_color(6, rgb2.r, rgb2.g, rgb2.b);
-
-    // F7 - F8
-    rgb_matrix_set_color(42, rgb2.r, rgb2.g, rgb2.b);
-    rgb_matrix_set_color(43, rgb2.r, rgb2.g, rgb2.b);
-
-    // W
-    rgb_matrix_set_color(18, rgb2.r, rgb2.g, rgb2.b);
-    // A S D ?
-    rgb_matrix_set_color(24, rgb2.r, rgb2.g, rgb2.b);
-    rgb_matrix_set_color(25, rgb2.r, rgb2.g, rgb2.b);
-    rgb_matrix_set_color(26, rgb2.r, rgb2.g, rgb2.b);
-
-
-    // < - ?
-    rgb_matrix_set_color(77, 255, 0, 0);
-    rgb_matrix_set_color(78, 255, 0, 0);
-    rgb_matrix_set_color(79, 255, 0, 0);
-    // right windows
-    rgb_matrix_set_color(86, 255, 0, 0);
-
-    // 6 - 2 ?
-    // rgb_matrix_set_color(10, rgb2.r, rgb2.g, rgb2.b);
-    // rgb_matrix_set_color(11, rgb2.r, rgb2.g, rgb2.b);
-    // rgb_matrix_set_color(12, rgb2.r, rgb2.g, rgb2.b);
-    // rgb_matrix_set_color(13, rgb2.r, rgb2.g, rgb2.b);
-    // rgb_matrix_set_color(14, rgb2.r, rgb2.g, rgb2.b);
-
-    // M2 or down ?
-    // rgb_matrix_set_color(15, 255, 255, 255);
-
-
-    // R or right space?
-    // rgb_matrix_set_color(20, 255, 0, 0);
-
-    // left shift or '"
-    // rgb_matrix_set_color(30, 0, 255, 0);
-
-    // FN or O ?
-    //rgb_matrix_set_color(40, 0, 0, 255);
-
-    // I
-    // rgb_matrix_set_color(60, 0, 255, 255);
-
-    // right shift
-    // rgb_matrix_set_color(80, 255, 255, 255);
-
-    // L
-    // rgb_matrix_set_color(70, 255, 0, 255);
 
     return rgb_matrix_check_finished_leds(led_max);
 }
